49d3109c7d93aafded32c82d368af4b9688dfcaa
Dayou Zhang <zhan6350@umn.edu>
2023-09-05 22:58:45 -0500

    fix dpem

--- chempotpy/gen_pyf.sh
+++ chempotpy/gen_pyf.sh
@@ -2,8 +2,9 @@
 
 # Script to generate .pyf file for new surfaces
 # Dayou Zhang, Aug 30, 2023
+# updated Sept 5, 2023
  
 src=`basename $1`
 module=${src%.*}
 cd `dirname $1`
-f2py only: pes : -m ${module} -h ${module}.pyf $src
+f2py only: pes dpem : -m ${module} -h ${module}.pyf $src
